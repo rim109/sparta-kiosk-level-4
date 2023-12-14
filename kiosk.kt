f414fdf6a2f47890327cf7517526e22025cdb9c7
rim109 <132591560+rim109@users.noreply.github.com>
2023-12-14 23:11:33 +0900

    feat: text 수정

--- kiosk.kt
+++ kiosk.kt
@@ -59,7 +59,7 @@ open class Kiosk {
             println("\u001B[32m아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.\u001B[0m")
             println("\u001B[32m[ Menu ]\u001B[0m")
             println(
-                "1. Burgers         | 앵거스 비프 통살을 다져만든 버거\n" +
+                        "1. Burgers         | 앵거스 비프 통살을 다져만든 버거\n" +
                         "2. Forzen Custard  | 매장에서 신선하게 만드는 아이스크림\n" +
                         "3. Drinks          | 매장에서 직접 만드는 음료\n" +
                         "4. Beer            | 뉴욕 브루클린 브루어리에서 양조한 맥주\n" +
@@ -158,7 +158,7 @@ class Hamburger() : AbstractMenu() {
                         println("----------------------------------------------------------------------------------------------")
                         println("\u001B[32m[ Total ]\u001B[0m ${burgerMenus[Number - 1].Price}원")
 
-                        println("1. 지불 2. 취소")
+                        println("1. 지불하기 2. 장바구니 취소")
                         var select = readln().toInt()
                         var bags = "${burgerMenus[Number - 1].Name} | ${burgerMenus[Number - 1].Price}"
                         if (select == 2) {
